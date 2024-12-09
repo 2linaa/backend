9823aef70cbfc4e761175ad88c366f920c6bd876
2linaa <mbananalinaa@gmail.com>
2024-12-09 13:34:53 +0700

    fixing2

--- index.js
+++ index.js
@@ -94,9 +94,9 @@ app.get('/', (req, res) => {
     res.send('API berjalan!');
 });
 
-// Jalankan server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
+Jalankan server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
